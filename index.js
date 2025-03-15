ba2daa8987c22bce87e7549d4f81d6a230deca85
Juan Felipe Rojas <jrojash10040@universidadean.edu.co>
2025-03-14 22:08:24 -0500

    Carrusel para la parte de experiencia profesional

---
+++ index.js
@@ -0,0 +1,24 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove("active");
+        if (i === index) {
+            slide.classList.add("active");
+        }
+    });
+}
+
+function moveSlide(step) {
+    currentSlide += step;
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    } else if (currentSlide < 0) {
+        currentSlide = slides.length - 1;
+    }
+    showSlide(currentSlide);
+}
+
+
+showSlide(currentSlide);
